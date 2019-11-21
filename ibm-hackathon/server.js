ac47541dba1b036ccbe21998acb144dfb493994d
Jeroen Groenveld <jeroen.groenveld@j20.nl>
2019-11-21 15:09:43 +0100

    WIP

--- ibm-hackathon/server.js
+++ ibm-hackathon/server.js
@@ -1,16 +1,16 @@
-var http = require('http');
+http = require('http');
 var fs = require('fs');
 var path = require('path');
 
-http.createServer(function (request, response) {
-    console.log('request starting...');
+//create a server object:
+http.createServer(function (req, res) {
 
-    var filePath = './dist' + request.url;
-    if (filePath == './dist/')
-        filePath = './dist/index.html';
-
-    console.log(filePath);
-
+    var path = './dist';
+    if (req.url === '/') {
+        path += '/index.html';
+    } else {
+        path += req.url;
+    }
 
     var extname = path.extname(filePath);
     var contentType = 'text/html';
@@ -21,40 +21,19 @@ http.createServer(function (request, response) {
         case '.css':
             contentType = 'text/css';
             break;
-        case '.json':
-            contentType = 'application/json';
-            break;
         case '.png':
             contentType = 'image/png';
             break;
         case '.jpg':
             contentType = 'image/jpg';
             break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
     }
 
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT'){
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                response.end();
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.write(content, 'utf-8');
-            response.end();
-        }
-    });
+    const content = fs.readFileSync(path);
 
-}).listen(8080);
+    res.writeHead(200, {'Content-Type': contentType});
+    res.write(content); //write a response to the client
+    res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
+
 console.log('Server running at http://127.0.0.1:8080/');
